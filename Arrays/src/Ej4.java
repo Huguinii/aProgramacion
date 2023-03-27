8410e76858c03439e56d643fd88773fb59813b44
Hugo <hugomnbn@gmail.com>
2023-03-27 17:54:09 +0200

    commit general

---
+++ Arrays/src/Ej4.java
@@ -0,0 +1,11 @@
+public class Ej4 {
+    public static void main(String[] args) {
+        int[] numero = new int[20];
+        int[] cuadrado = new int[20];
+        int[] cubo = new int[20];
+        for (int i = 0; i < ; i++){
+
+        }
+    }
+
+}
